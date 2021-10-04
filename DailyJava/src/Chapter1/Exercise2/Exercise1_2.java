d71151bcdea4895ab3881b5bde3412b40dd000f6
Paweu <pduminski@gmail.com>
2021-10-04 07:37:45 +0200

    commit of Exercise 2 and start of Exercise 3

--- DailyJava/src/Chapter1/Exercise2/Exercise1_2.java
+++ DailyJava/src/Chapter1/Exercise2/Exercise1_2.java
@@ -1,4 +1,8 @@
 package Chapter1.Exercise2;
 
 public class Exercise1_2 {
+    public static void main(String[] args) {
+        System.out.println("Program will show the value of PI with the given extension");
+        System.out.printf("Pi = " + "%6.5f\n", Math.PI);
+    }
 }
---
+++ DailyJava/src/Chapter1/Exercise3/Exercise1_3.java
@@ -0,0 +1,7 @@
+package Chapter1.Exercise3;
+
+public class Exercise1_3 {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
